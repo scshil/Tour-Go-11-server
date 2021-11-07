43921bb56255893e8841a326ea9286134e288068
scshil <chandrasaurav20@gmail.com>
2021-11-07 06:23:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const cors = require("cors");
+const Objectid = require("mongodb").ObjectId;
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 7000;
+// middleware
+app.use(cors());
+app.use(express.json());
+//
+app.get("/", (req, res) => {
+  res.send("hi api");
+});
+// connection
+const { MongoClient } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s1mcl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+//
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("travel");
+    const serviceCollection = database.collection("services");
+    const orderCollection = database.collection("orders");
+    //   get data from database
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // send data from ui
+    app.post("/addservice", async (req, res) => {
+      const service = req.body;
+      //   console.log("hit api", service);
+      const postResult = await serviceCollection.insertOne(service);
+      res.json(postResult);
+    });
+    // get single product
+    app.get("/services/:id", async (req, res) => {
+      // console.log(req.params.id);
+      const singleresult = await serviceCollection
+        .find({ _id: Objectid(req.params.id) })
+        .toArray();
+      res.send(singleresult[0]);
+    });
+    // ordered producr
+    app.post("/purchesitem", async (req, res) => {
+      const order = req.body;
+      const postorder = await orderCollection.insertOne(order);
+      res.send(postorder);
+    });
+    // getting product using email
+    app.get("/purchesitem/:email", async (req, res) => {
+      const cursor = orderCollection.find({ email: req.params.email });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // delete
+    app.delete("/delete/:id", async (req, res) => {
+      const result = await orderCollection.deleteOne({
+        _id: Objectid(req.params.id),
+      });
+      res.send(result);
+    });
+    /* winter service */
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+//
+app.listen(port, () => {
+  console.log("connected! in ", port);
+});
